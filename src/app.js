d0df0f2ae2431a1ea68d29ac255e81be6b8d5c6f
elvisuriel <urielmarciales@gmil.com>
2025-03-22 15:33:37 -0500

    mas ajustes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import http from "http"; 
-import { uploadFile } from "./controllers/uploadS3Controller.js"; 
-import routes from "./routes/index.js"; 
+import http from "http";
+import { uploadFile } from "./controllers/uploadS3Controller.js";
+import routes from "./routes/index.js";
 import setupWebSocket from "./sockets/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 4005; 
+const PORT = process.env.PORT || 4005;
 const app = express();
-const server = http.createServer(app); 
+const server = http.createServer(app);
 
 setupWebSocket(server); // Configura WebSocket
 
@@ -18,7 +18,7 @@ setupWebSocket(server); // Configura WebSocket
 app.use(cookieParser());
 app.use(
   cors({
-     origin: ["https://a.app.esturio.com","http://localhost:5173", "http://localhost:3000", "https://landing.app.esturio.com" ],
+    origin: ["https://a.app.esturio.com", "http://localhost:5173", "http://localhost:3000", "https://landing.app.esturio.com"],
   })
 );
 app.use(express.json());
@@ -31,10 +31,9 @@ app.use((err, req, res, next) => {
   console.error("Error:", err.message);
   res.status(500).send("Error interno del servidor");
 });
-server.listen(PORT, () => {
-  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor en ejecución en http://0.0.0.0:${PORT}`);
 });
-
 
 
 app.get("/", (req, res) => {
