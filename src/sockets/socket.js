d6bf8b355bc70e191bc687e4d9c3c434c4ed5658
elvisuriel <urielmarciales@gmil.com>
2025-04-22 21:10:02 -0500

    mas aj ustes cors

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -5,11 +5,19 @@ import { initWhiteboard, initChat } from "./index.js";
 const setupWebSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: true,
+      origin: [
+        "http://azanavariedades.shop",  // Tu dominio frontend
+        "http://54.67.30.105:3001",     // Tu backend
+        "https://esturiofron.netlify.app", // Frontend en Netlify
+        "http://localhost:3000", // Para desarrollo local
+        "https://landing.app.esturio.com"  // Otro dominio de la app
+      ],
       methods: ["GET", "POST"],
+      credentials: true  // Habilita el uso de cookies y credenciales si es necesario
     }
   });
 
+  // Inicializa la pizarra y el chat con las conexiones de Socket.io
   initWhiteboard(io);
   initChat(io);
 }
